020371ce7c1693de452e2cf4e260c51736eefe98
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2017-09-27 09:04:29 +0200

    Concurrency

--- src/main/java/pl/training/concurrency/downloader/Downloader.java
+++ src/main/java/pl/training/concurrency/downloader/Downloader.java
@@ -1,6 +1,8 @@
 package pl.training.concurrency.downloader;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 import java.io.IOException;
@@ -20,10 +22,56 @@ public class Downloader {
                 .observeOn(Schedulers.io())
                 .subscribeOn(Schedulers.io())
                 .doOnNext(outputStream::write)
-                .observeOn(Schedulers.from(swingExecutor))
                 .map(bytes -> (long) bytes.length)
+                .observeOn(Schedulers.from(swingExecutor))
                 .subscribe(onData::accept, Throwable::printStackTrace, outputStream::close);
         return observableBytesStream::stop;
     }
+
+/*
+    public Stoppable download(URL url, OutputStream outputStream, Consumer<Long> onData) throws IOException {
+        ObservableBytesStream observableBytesStream = new ObservableBytesStream(url, BUFFER_SIZE);
+        Observable.create(observableBytesStream)
+                .observeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
+                .doOnNext(new io.reactivex.functions.Consumer<byte[]>() {
+
+                    @Override
+                    public void accept(byte[] bytes) throws Exception {
+                        outputStream.write(bytes);
+                    }
+
+                })
+                .observeOn(Schedulers.from(swingExecutor))
+                .subscribe(new io.reactivex.functions.Consumer<byte[]>() {
+
+                               @Override
+                               public void accept(byte[] bytes) throws Exception {
+                                   onData.accept((long) bytes.length);
+                               }
+
+                           },
+                        new io.reactivex.functions.Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                throwable.printStackTrace();
+                            }
+                        },
+                        new Action() {
+                            @Override
+                            public void run() throws Exception {
+                                outputStream.close();
+                            }
+                        });
+        return new Stoppable() {
+
+            @Override
+            public void stop() {
+                observableBytesStream.stop();
+            }
+
+        };
+    }*/
+
 
 }
